276db582ef12a5ef4c15b1e1c48cd9ceb6fbd010
Parth <parth.sce21@sot.pdpu.ac.in>
2024-10-12 19:10:43 +0530

    configuring drizzle ORM and Neon

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,29 +1,32 @@
 import {Hono} from 'hono';
 import {handle} from 'hono/vercel';
-import {z} from "zod";
-import { zValidator } from '@hono/zod-validator';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
-import { error } from 'console';
  
-
+ 
+import  accounts  from "./accounts"
 export const runtime ='edge';
 
 const app = new Hono().basePath('/api')
+// app.route("/accounts",accounts)
+const routes = app
+.route("/accounts",accounts);
+export const GET = handle(app);
+export const POST = handle(app);
 
-app.get('/hello', (c)=>{
-    // clerkMiddleware(),
-    // helloController(),
-    // (c)=>{
+export type AppType = typeof routes;
+// app.get('/hello', (c)=>{
+//     // clerkMiddleware(),
+//     // helloController(),
+//     // (c)=>{
     
-    //     const auth = getAuth(c);
-    //     if(!auth?.userId){
-    //         return c.json({
-    //             error:"Unauthorized!!"
-    //         })
-    //     }
-    return c.json({
-            message:'Hello NextJs',})
-    })
+//     //     const auth = getAuth(c);
+//     //     if(!auth?.userId){
+//     //         return c.json({
+//     //             error:"Unauthorized!!"
+//     //         })
+//     //     }
+//     return c.json({
+//             message:'Hello NextJs',})
+//     })
 // bellow is the best wasy to do the api ( practice )
 // .get(
     
@@ -60,5 +63,3 @@ app.get('/hello', (c)=>{
 // app.route('/authors',authors);
 // app.route('/books',books);
 
-export const GET = handle(app);
-export const POST = handle(app);
